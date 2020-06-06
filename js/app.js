499677b128b5c0fccdd0643a7e41947b37246b68
Lex <lexcpryor@gmail.com>
2020-06-05 19:41:33 -0500

    script with id tags on index

--- js/app.js
+++ js/app.js
@@ -1,10 +1,7 @@
-
 // Cocktail Search by Liquor function
-
-$("#l").on("click", function () {
-
-  // var search = // Value of search form, Convert to string?
-  search = document.getElementById("searchLiquor").value;
+$("#choiceSubmit").on("click", function () {
+  event.preventDefault();
+  search = document.getElementById("liquorSelect").value;
   var queryURL = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + search;
 
   $.ajax({
@@ -12,47 +9,27 @@ $("#l").on("click", function () {
     method: "GET"
   })
     .then(function (response) {
-      $("#display").empty();
       results = response.drinks;
+
       //Math random for random drink to be displayed
-      var num = Math.floor(Math.random() * results.length); //Eliminate repeat?
+      var num = Math.floor(Math.random() * results.length); 
+      // Store drink for second api
       var drinkId = results[num].idDrink;
 
       //Drink Name Div
-      var liquorDiv = $("<div>")
-      liquorName = $("<h4>").text(results[num].strDrink)
+      $("#name").text(results[num].strDrink)
 
       // Drink Img
-      var drinkImg = $("<img>");
-      drinkImg.attr("src", results[num].strDrinkThumb);
-      drinkImg.attr("id", "drinkImg");
-      // drinkImg.attr("value", drinkId);
+      $("#cocktailImg").attr("src", results[num].strDrinkThumb);
 
-      //  var newDiv = $("<div>");
-      //  newDiv.attr("id", "recipeCard");
-
-
-      // Attach Image to drink name
-      liquorName.append(drinkImg);
-      liquorDiv.append(liquorName);
-
-      //append name and img to docuent
-      $("#drinkResults").append(liquorDiv);
-
-      // if strIngredient != null display = none? or diplay class with property none?
-      // SECOND FUNCTION
-
+      // SECOND API
       queryURL2 = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + drinkId;
-
-      // Get drink info from API
       $.ajax({
         url: queryURL2,
         method: "GET"
       })
         .then(function (response) {
-          console.log(response)
           results = response.drinks[0];
-
           var ingredients = [
             results.strIngredient1,
             results.strIngredient2,
@@ -88,6 +65,7 @@ $("#l").on("click", function () {
             results.strMeasure14,
             results.strMeasure15,
           ]
+          // Create a table for ingredients, measurements
           var i = 0
           ingredients.forEach(generateInfo)
 
@@ -100,19 +78,37 @@ $("#l").on("click", function () {
             newRow.append(ingDiv);
             newRow.append(measDiv);
 
-            $("#display").append(newRow)
-            
+            $("#cocktailRecipe").append(newRow)
+
+            //Iterate for measure
             i++;
-          }$("#recipeText").append(instruction)
-
-
-        })
+          } // Add instructions 
+          $("#cocktailRecipe").append(instruction)
+        });
     });
+
+
 });
+// PAGE ONE
+$("#ageSubmit").on("click", function drink() {
+  event.preventDefault();
+  optionPage.style.display = "block";
+  agePage.style.display = "none";
+})
 
-/*
- //Music Search Function
+//SELECT PAGE
+$("#choiceSubmit").on("click", function choice() {
+  event.preventDefault();
+  optionPage.style.display = "none";
+  resultsPage.style.display = "block";
+})
 
-  // CLICK DRINK TO DISPLAY RECIPE MODAL
-     //    $("#drinkImg").on("click", recipeModal);
-*/
+function clear() {
+  $("#display").empty();
+  $("#drinkResults").empty();
+}
+
+//Pages
+agePage = document.getElementById("legal");
+optionPage = document.getElementById("mixForm");
+resultsPage = document.getElementById("results");
