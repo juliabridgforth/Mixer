db03cab09cf4922fb19e77d6afbd0b558848241f
JuliaB <julia.d.bridgforth@vanderbilt.edu>
2020-06-08 11:27:55 -0400

    animation station

--- js/app.js
+++ js/app.js
@@ -1,1 +1,6 @@
-$(document).foundation()
+$(document).foundation();
+
+var dateControl = document.querySelector('input[type="date"]');
+dateControl.value = '2017-06-01';
+console.log(dateControl.value); // prints "2017-06-01"
+console.log(dateControl.valueAsNumber); // prints 1496275200000, a UNIX timestamp
